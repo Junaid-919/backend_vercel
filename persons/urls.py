7b5e26fe646f0ce0fd6fbbf178f74d5bb086959f
Junaid-919 <s.md.junaidd@gmail.com>
2026-01-20 19:24:06 +0530

    Update urls.py

--- persons/urls.py
+++ persons/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("get_person_data/", views.get_person_data),
-    path("create_person_data/", views.create_person_data),
-    path("update_person/<int:person_id>/", views.update_person),
-    path("delete_person/<int:person_id>/", views.delete_person),
-    path("get_person_withid/<int:person_id>/", views.get_person_withid),
+    path("get_person_data/", views.get_person_data, name="get_person_data"),
+    path("create_person_data/", views.create_person_data, name="create_person_data"),
+    path("update_person/<int:person_id>/", views.update_person, name="update_person"),
+    path("delete_person/<int:person_id>/", views.delete_person, name="delete_person"),
+    path("get_person_withid/<int:person_id>/", views.get_person_withid, name="get_person_withid"),
 ]
