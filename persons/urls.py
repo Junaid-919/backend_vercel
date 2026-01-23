9569d7da684551749cae4e2d5b5535f8005ce62b
Junaid-919 <s.md.junaidd@gmail.com>
2026-01-23 12:16:38 +0530

    Update urls.py

--- persons/urls.py
+++ persons/urls.py
@@ -7,11 +7,6 @@ urlpatterns = [
     path("update_person/<int:person_id>/", views.update_person, name="update_person"),
     path("delete_person/<int:person_id>/", views.delete_person, name="delete_person"),
     path("get_person_withid/<int:person_id>/", views.get_person_withid, name="get_person_withid"),
-<<<<<<< HEAD
-]
-=======
-
-
 
     path("get_location_data/", views.get_location_data, name="get_location_data"),
     path("create_location_data/", views.create_location_data, name="create_person_data"),
@@ -19,4 +14,3 @@ urlpatterns = [
     path("delete_location/<int:location_id>/", views.delete_location, name="delete_person"),
     path("get_location_withid/<int:location_id>/", views.get_location_withid, name="get_person_withid"),
 ]
->>>>>>> 47c1e98 (Initial commit jan23)
