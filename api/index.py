28d847ab3ca1bbf6761e23ef49dfb738eed9d473
Junaid-919 <s.md.junaidd@gmail.com>
2026-01-23 12:34:28 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pms.settings")
 
-<<<<<<< HEAD
 app = get_wsgi_application()   # 👈 MUST be named `app`
-=======
 app = get_wsgi_application()   # 👈 MUST be named `app`
->>>>>>> 47c1e98 (Initial commit jan23)
