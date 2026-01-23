41660ab71f581e271aea5d94a0e87488f5ffde43
Junaid-919 <s.md.junaidd@gmail.com>
2026-01-23 12:15:36 +0530

    Update serializers.py

--- persons/serializers.py
+++ persons/serializers.py
@@ -1,20 +1,13 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from persons.models import Person
-=======
 from persons.models import Person, Location
->>>>>>> 47c1e98 (Initial commit jan23)
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = "__all__"
-<<<<<<< HEAD
-=======
-
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = "__all__"
->>>>>>> 47c1e98 (Initial commit jan23)
