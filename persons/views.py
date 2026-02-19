20521bd46fad46be5ec5589f73608e363e8bd507
Junaid-919 <s.md.junaidd@gmail.com>
2026-02-19 14:55:05 +0530

    Update views.py

--- persons/views.py
+++ persons/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from persons.models import Person
 from persons.serializers import PersonSerializer, BusStopSerializer, BusServiceSerializer
-from persons.models import Person, Location
+from persons.models import Person, Location, BusStop, BusService
 from persons.serializers import PersonSerializer, LocationSerializer
 
 
