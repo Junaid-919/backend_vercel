997de2e858c6499270c7e3510c1d9e4fdf68566c
Junaid-919 <s.md.junaidd@gmail.com>
2026-02-19 15:46:48 +0530

    Update urls.py

--- pms/urls.py
+++ pms/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from persons.views import health
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("persons.urls")),
-    path("", health),
 ]
