d9577f9bc5fa6e8b1b33c142e80f32b9095dceb4
devsmd <devuser024@gmail.com>
2026-02-19 14:32:57 +0530

    name alter commit

--- pms/settings.py
+++ pms/settings.py
@@ -11,7 +11,11 @@ SECRET_KEY = "django-insecure-ceqg5snn)m&l9ww5-p2surc=ybw5*^8(2q$!*(_&coqk64w_!v
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["*"]
+# CORS_ALLOWED_ORIGINS = ["*"]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://vercel-wpf2.vercel.app",
+]
 
 # Application definition
 
@@ -101,7 +105,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CSRF_TRUSTED_ORIGINS = ["https://*.vercel.app"]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
